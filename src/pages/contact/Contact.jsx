17c40b9a746b26c9ee34c6cd8fd48af24aa88ada
H. Nazmul Hassan <Nazmul.w3@gmail.com>
2021-11-06 19:17:43 +0600

    contact us before pulling

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../../Components/Header/Header';
+import ContactUsForm from './ContactUsForm';
 
 const Contact = () => {
     return (
         <main id="contact-page">
             <Header/>
+            <ContactUsForm/>
         </main>
     );
 };
--- src/pages/contact/ContactUsForm.jsx
+++ src/pages/contact/ContactUsForm.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const ContactUsForm = () => {
     return (
-        <section id="Contact Us Form">
-            
+        <section id="contact-us-form">
+            <div className="container-xxl">
+                <div className="form-area"></div>
+            </div>
         </section>
     );
 };
