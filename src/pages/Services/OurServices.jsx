b32b6cba0f27e312ec66648574915724b36ce75a
H. Nazmul Hassan <Nazmul.w3@gmail.com>
2021-11-09 13:17:38 +0600

    before pulling from Shourov

--- src/pages/Services/OurServices.jsx
+++ src/pages/Services/OurServices.jsx
@@ -3,6 +3,12 @@ import SectionTitle from "../../Components/SectionTitle/SectionTitle";
 import ServicesCard from "../../Components/ServicesCard/ServicesCard";
 
 const ServicesSection = () => {
+    const sliders = 5;
+
+    /* useEffect(() => {
+        setSliderHeight(document.getElementById("services-animation-card-slider").clientHeight);
+    }) */
+
     return (
         <section id="services_section">
             <div className="container-xxl">
@@ -19,6 +25,16 @@ const ServicesSection = () => {
                         ))}
                     </div>}
                 </div>
+
+                {/* <div className="slider-wrapper">
+                    <section id="services-animation-card-slider" className="slider-pages">
+                        {[...new Array(sliders)].map((el, i) => (
+                            <article className={`js-scrolling__page js-scrolling__page-${i+1} ${i===0?"js-scrolling--active":""}`}>
+                                <div className="services-card">{i+1}</div>
+                            </article>
+                        ))}
+                    </section>
+                </div> */}
             </div>
         </section>
     );
